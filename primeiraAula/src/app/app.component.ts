e296d84af34606330b7dae99d7cf274d0607620f
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2018-08-13 20:08:28 -0300

    Add ngFor

--- primeiraAula/src/app/app.component.ts
+++ primeiraAula/src/app/app.component.ts
@@ -11,6 +11,30 @@ export class AppComponent {
 
   myCar : Car = new Car();
   
+  cars : Car[] = [];
+
+  ngOnInit() {
+    this.myCar.brand = 'Fiat';
+    this.myCar.color = 'Cinza';
+    this.myCar.name = 'Palio';
+    this.myCar.price = 'R$ 17.523,87';
+    this.myCar.year = 2012;
+    this.createFakeCars();
+  }
+
+  createFakeCars() {
+    let year = 2010;
+    for (let i = 0; i < 5; i++) {
+      let fakeCar = new Car();
+      fakeCar.brand = 'Fiat';
+      fakeCar.color = 'Cinza';
+      fakeCar.name = 'Palio';
+      fakeCar.price = 'R$ 15.000,00';
+      fakeCar.year = year++;
+      
+      this.cars.push(fakeCar);
+    }
+  }
 
   
 }
